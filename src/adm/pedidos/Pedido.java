0321e9266457715571cebe4b2e4566f5edc38557
Moises Apaza <dotty@live.com.ar>
2018-10-19 13:28:59 -0300

    primer commit agregando class pedido

---
+++ src/adm/pedidos/Pedido.java
@@ -0,0 +1,23 @@
+package adm.pedidos;
+
+public class Pedido {
+	private Integer numeroPedido;
+	private Integer cantidad;
+	
+	public void condultarPedido() {
+	
+	}
+	
+	public void cancelarPedido(){
+		
+	}
+	
+	public void confirmarPedido() {
+		
+	}
+	
+	public void modificarCantidad(){
+		
+	}
+	
+}
